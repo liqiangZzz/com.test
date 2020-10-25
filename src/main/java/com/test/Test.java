9ace14a1c118aa7505b760a1b1125b5a7586cecb
liqiang <liqiang@qq.com>
2020-10-25 21:15:37 +0800

    3

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("011111");//11111
         System.out.println("111111");
+        System.out.println("22222");
     }
 }
